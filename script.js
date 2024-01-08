031abee272bfbf49c9d27647e1c19275db0440db
Fedz00 <155079961+Fedz00@users.noreply.github.com>
2024-01-08 19:47:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const backText = document.getElementById('backText');
+
+    document.addEventListener('mousemove', function (event) {
+        // Check if the mouse is on the left side of the page
+        if (event.clientX < window.innerWidth / 2) {
+            backText.style.opacity = 1;
+            backText.style.top = `${event.clientY}px`;
+        } else {
+            backText.style.opacity = 0;
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        // Check if the click is on the left side of the page
+        if (event.clientX < window.innerWidth / 2) {
+            history.back();
+        }
+    });
+});
